da874ff6253b3be1bb98d6a0f928e0a8cb2d987d
tonhwk <andres_btk@yahoo.com>
2020-08-01 15:56:17 -0500

    implementing firebase utils including ability to store authenticated users into firestore

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -6,7 +6,7 @@ import SignPage from "./pages/signpage/signpage.component"
 import Header from "./components/header/header.component"
 
 import { Switch, Route } from "react-router-dom"
-import { auth } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
 class App extends React.Component {
   constructor() {
@@ -17,10 +17,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          })
+          console.log(this.state)
+        })
+      }
+      this.setState({ currentUser: userAuth })
     })
   }
+
   componentWillUnmount() {
     this.unsubscribeFromAuth()
   }
--- crwn-clothing/src/pages/signpage/signpage.component.jsx
+++ crwn-clothing/src/pages/signpage/signpage.component.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import SignIn from "../../components/sign-in/sign-in.component"
+import SignUp from "../../components/sign-up/sign-up.component"
+
+import "./signpage.styles.scss"
 
 const SignPage = () => (
-  <div className='sign-in'>
+  <div className='sign-page'>
     <SignIn />
+    <SignUp />
   </div>
 )
 
